4332e46f487f13c30471c0272c22b1a607bc8efe
Arthur David <32845935+arthurdlima@users.noreply.github.com>
2020-04-30 01:48:45 -0300

    starting header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,25 @@ function Header() {
         <Fragment>
             <header>
                 <div className='header-container'>
-                    <img src={Logo} alt="King AD Shop Logo"/>
+                    <img src={Logo} alt="King AD Shop Logo" />
+                    <div>
+                        <nav>
+                            <ul>
+                                <li>
+                                    <a href="#">Home</a>
+                                </li>
+                                <li>
+                                    <a href="#">Products</a>
+                                </li>
+                                <li>
+                                    <a href="#">About</a>
+                                </li>
+                                <li>
+                                    <a href="#">Contact</a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
                 </div>
             </header>
         </Fragment>
