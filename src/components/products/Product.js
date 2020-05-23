7bff269e0c2859c59f597372fb6b2620d8d2e006
Arthur David <32845935+arthurdlima@users.noreply.github.com>
2020-05-23 20:31:27 -0300

    working on fetch

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 function Product(img,price,prodName) {
-
+    img = 'some image';
+    price = '$200';
+    prodName = 'watch';
     return (
         <div className='product'>
-            <img src={img} alt='product image'/>
+            <img src={img} alt='product'/>
             <p>{price}</p>
             <p>{prodName}</p>
         </div>
--- src/components/products/ProductsSection.js
+++ src/components/products/ProductsSection.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import SearchBox from './SearchBox';
+import Product from './Product';
 
 function ProductsSection() {
+    const result = getProducts();
 
     return (
         <div>
             <SearchBox />
             <section className='products-grid'>
-
+                <Product />
             </section>
         </div>
     );
 }
+
+function getProducts(someSearchText) {
+    fetch('http://localhost:3000/products-data/product-list.json')
+        .then(res => res.json())
+        .then(data => console.log(data));
+}
+
+
 
 export default ProductsSection;
