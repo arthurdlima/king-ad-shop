bec97639888bfe414d41e4d0ace8ac73e84f3db7
Arthur David <32845935+arthurdlima@users.noreply.github.com>
2020-05-24 04:12:21 -0300

    product card finished

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
 function Product(img,price,prodName) {
-    img = 'some image';
+    img = 'http://localhost:3000/products-data/images/1-watches/watch_1_small.jpg';
     price = '$200';
     prodName = 'watch';
     return (
         <div className='product'>
-            <img src={img} alt='product'/>
-            <p>{price}</p>
-            <p>{prodName}</p>
+            <img src={img} alt='product' />
+            <div>
+                <p>{prodName}</p>
+                <p>{price}</p>
+            </div>
         </div>
     );
 }
